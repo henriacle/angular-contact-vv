dadc969e18e17e94ee4b2183a59fcf32a0e6c5cc
Henrique <henrique.clementino@gps-pamcary.com.brr>
2018-08-06 14:59:01 -0300

    Removido os imports do app.module após inserir  lazy load

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from '@app/app.component';
 import { AppRoutingModule } from '@app/app-routing.module';
-import { ContatoModule } from '@app/containers/contato/contato.module';
 import { SharedModule } from '@app/shared/shared.module';
-import { MensagensModule } from '@app/containers/mensagens/mensagens.module';
 
 @NgModule({
   declarations: [
@@ -13,8 +11,6 @@ import { MensagensModule } from '@app/containers/mensagens/mensagens.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ContatoModule,
-    MensagensModule,
     SharedModule
   ],
   providers: [],
